0dc4ea1616895361ee955c49c4c7429b5673828a
Martin Mwangi <mwangimartin1904@gmail.com>
2023-09-01 22:43:45 +0300

    Update configuration and read a collection

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,17 +1,40 @@
 // keystatic.config.ts
-import { config, fields, singleton } from '@keystatic/core';
+import { collection, config, fields, singleton } from '@keystatic/core';
 
 export default config({
 	storage: {
 		kind: 'local',
 	},
+
 	singletons: {
 		homepage: singleton({
-			label: 'Homepage',
-			path: 'src/content/_homepage',
+			label: 'HomeTitle',
+			path: 'src/content/homepage',
 			schema: {
-				headline: fields.text({ label: 'Headline' })
+				headline: fields.text({ label: 'HomeText' }),
 			},
 		}),
 	},
+
+	collections: {
+		articles: collection({
+			slugField: "path",
+			label: 'Articles',
+			path: 'src/content/collections/articles/*/',
+			schema: {
+				path: fields.slug({ name: { label: 'path' } }),
+				title: fields.text({ label: 'Title' }),
+				cover: fields.image({ label: 'Cover Image', }),
+				date: fields.date({ label: 'Date' }),
+				text: fields.document({
+					label: 'Text',
+					formatting: true,
+					links: true,
+					dividers: true,
+					tables: true,
+					images: true,
+				}),
+			}
+		}),
+	}
 });
